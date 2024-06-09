9397dfd28f4ff12cb80d770a8cd04c6f1fb3f664
AnakinKenneth <anakin.flores0903@gmail.com>
2024-06-08 23:36:02 -0500

    first commit

---
+++ src/main/java/pe/edu/cibertec/DswiiCl1SoapFloresanakinApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiCl1SoapFloresanakinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiCl1SoapFloresanakinApplication.class, args);
+	}
+
+}
